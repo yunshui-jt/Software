6f3722e8d61bad60ccb6b5ae50f120313ca3e088
yunshui-jt <55734102+yunshui-jt@users.noreply.github.com>
2019-12-25 12:27:27 +0800

    Add files via upload

---
+++ LoadContent/Load.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LoadContent
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Loaddown : Window
+    {
+        public Loaddown()
+        {
+            InitializeComponent();
+            u = new Model1();
+            this.DataContext = u;
+        }
+
+        private Model1 u;
+        private void bt_LoadText_Click(object sender, RoutedEventArgs e)
+        {
+            string path = FindFileDialog("txt files(*.txt)|*.txt");
+            tb_path.Text = path;
+            string content = u.ReadFile(path, u.Encodings[cbx_encode.SelectedIndex]);
+            u.Content = content;
+        }
+
+        public string FindFileDialog(string filter)
+        {
+            //文件全路径
+            string localFilePath = "";
+            try
+            {
+                Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+                openFileDialog.Filter = filter;// "txt files(*.txt)|*.txt";
+                openFileDialog.RestoreDirectory = true;
+                bool? result = openFileDialog.ShowDialog();
+
+                if (result == true)
+                {
+                    //获得文件路径
+                    return localFilePath = openFileDialog.FileName.ToString();
+                }
+                return localFilePath;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("所选上传文件不正确，请重新选！");
+                return localFilePath;
+            }
+        }
+
+        private void bt_Ok_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(tb_content.Text))
+            {
+                Model1.ContentwithLoad = tb_content.Text;
+            }
+            this.Close();
+        }
+
+        private void bt_Cancle_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ LoadContent/Model1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadContent
+{
+    public class Model1 : INotifyPropertyChanged
+    {
+        public static string ContentwithLoad = string.Empty;
+        private string _content;
+        public string Content
+        {
+            get => _content;
+            set
+            {
+                if (value == _content) return;
+                _content = value;
+                OnPropertyChanged(nameof(Content));
+            }
+        }
+
+        private string _txtpath;
+        public string txtpath
+        {
+            get => _txtpath;
+            set
+            {
+                if (value == _txtpath) return;
+                _txtpath = value;
+                OnPropertyChanged(nameof(txtpath));
+            }
+        }
+
+        
+        private static Encoding[] _Encodings = new Encoding[] { Encoding.Default, Encoding.ASCII, Encoding.UTF8, Encoding.Unicode, Encoding.UTF7, Encoding.UTF32, Encoding.BigEndianUnicode };
+        public Encoding[] Encodings { get { return _Encodings; } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string aPropertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(aPropertyName));
+
+        public string ReadFile(string path,Encoding encoding)
+        {
+            StreamReader sr = new StreamReader(path, encoding);
+            return sr.ReadToEnd();
+        }
+    }
+}
